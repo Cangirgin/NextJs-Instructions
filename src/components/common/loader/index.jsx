14269a134bad27de1051ad7102bf5b15b08bc4b1
Can Girgin <cangirrgin@gmail.com>
2024-05-13 14:01:22 +0300

    update

--- src/components/common/loader/index.jsx
+++ src/components/common/loader/index.jsx
@@ -1,16 +1,11 @@
-"use client"
-
-import Link from 'next/link'
 import React from 'react'
-import { Image, Spinner } from 'react-bootstrap'
+import { Spinner } from 'react-bootstrap'
 
 
 const Loader = () => {
     return (
         <>
-
             <Spinner animation='border' size='lg' />
-
         </>
     )
 }
